0be861132ceba116c029afaf5e38994ace7bbab1
Aakash <kanduaakash43@gmail.com>
2025-09-17 14:41:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+import health_server
 import asyncio, logging
 from config import Config
 from pyrogram import Client as VJ, idle
@@ -73,3 +73,4 @@ if __name__ == "__main__":
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
